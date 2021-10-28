d8149d161ce1289401d92bc327caa0893255efee
Ethan Mori <emori1248@gmail.com>
2021-10-28 17:55:13 -0400

    Fixed version number

--- main/bot.py
+++ main/bot.py
@@ -19,7 +19,7 @@ activity = discord.Game(name="Fuckers put me on AWS")
 bot = commands.Bot(command_prefix='~', intents=intents, activity=activity)
 
 generic_error = "Something broke, msg Potato (See console)."
-version = "0.1.2"
+version = "0.1.3"
 
 
 
