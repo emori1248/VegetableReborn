8970e235eb66ce3e6f66593f6323c1682c9f5ac7
Ethan Mori <emori1248@gmail.com>
2021-10-29 00:26:32 -0400

    Updated to use discordpy Client instead of Bot

--- main/bot.py
+++ main/bot.py
@@ -21,10 +21,23 @@ activity = discord.Game(name="Fuckers put me on AWS")
 
 client = discord.Client(intents=intents, activity=activity)
 generic_error = "Something broke, msg Potato (See console)."
-version = "0.1.2"
+version = "0.2.0"
+
+# def add_command(func):
+#     commands.append(SimpleCommand(name='test', executor=func))
+
+class CommandHandler(object):
+    def __init__(self, name, desc='default description.'):
+        self.name = name
+        self.description = desc
+
+    def __call__(self, f):
+        commands.append(SimpleCommand(name=self.name, description=self.description, executor=f))
+
+
 
 class SimpleCommand:
-    def __init__(self, name, executor, description='default description'):
+    def __init__(self, name, executor, description):
         self.name = name
         self.description = description
         self.executor = executor
@@ -61,79 +74,44 @@ get the next event from goog calendar and set a timer to post that to all regist
 on event fire set timer for next event
 """
 
-# # Generic Commands
-# @bot.command(name='image')
-# async def imageCommand(ctx, arg):
-#     if (await imagemod.addCenteredTextToImage(arg)):
-#         await ctx.send(file=discord.File(r'tmp/output.jpg'))
-#     else:
-#         await ctx.send(generic_error)
-#         print("ERROR: img dir empty for image command")
+@CommandHandler(name='image')
+async def imageCommand(ctx, arg):
+    if (await imagemod.addCenteredTextToImage(arg)):
+        await ctx.send(file=discord.File(r'tmp/output.jpg'))
+    else:
+        await ctx.send(generic_error)
+        print("ERROR: img dir empty for image command")
 
-# # Generic Commands
-# @bot.command(name='help')
-# async def helpCommand():
-#     print("beep")
-#     print(bot.commands)
+# TODO make better
+@CommandHandler(name='help')
+async def helpCommand(message, args):
+    print("test")
+    str = ""
+    for command in commands:
+        str += f'{command.name}: \'{command.description}\'\n'
+    await message.channel.send(str)
 
 # TTS
 
-# @bot.command(name='tts')
-# async def testCommand(ctx):
-#     # filename = tts.synthesize_text(arg)
-#     # await music.play_file(ctx, filename, bot)
-#     await ctx.send("TTS is currently disabled due to reaching Google's TTS quota.")
-#     # Ran out of google TTS api quota lmao might switch
+@CommandHandler(name='tts', desc='Reads a user supplied message')
+async def ttsCommand(message, args):
+    await message.channel.send("TTS is currently disabled due to reaching Google's TTS quota.")
+    # filename = tts.synthesize_text(arg)
+    # await music.play_file(ctx, filename, bot)
 
-# # Admin Commands
+# Admin Commands
 
-# @bot.command(name='version')
-# async def versionCommand(ctx):
-#     await ctx.send(f'Bot is running version {version}.')
-
-# @bot.command(name='timeout')
-# async def timeoutCommand(ctx):
-#     member = ctx.message.author
-#     role = discord.utils.get(ctx.message.guild.roles, name="Timeout")
-#     await member.add_roles(role)
-
-# Music Commands
-
-# @bot.command(name='test')
-# async def testCommand(ctx):
-#     content = calendarapi.main()
-    
-#     await ctx.send(content)
-
-# @bot.command(name='join',help='Joins the bot into the channel')
-# async def join(ctx):
-#     await music.join(ctx)
-
-# @bot.command(name='leave', help='Kicks the bot from the channel')
-# async def leave(ctx):
-#     await music.leave(ctx)
-
-# @bot.command(name='play', help='To play song')
-# async def play(ctx, url):
-#     await music.play(ctx, url, bot)
-
-# @bot.command(name='pause', help='This command pauses the song')
-# async def pause(ctx):
-#     await music.pause(ctx)
-
-# @bot.command(name='resume', help='Resumes the song')
-# async def resume(ctx):
-#     await music.resume(ctx)
-
-# @bot.command(name='stop', help='Stops the song')
-# async def stop(ctx):
-#     await music.stop(ctx)
+@CommandHandler(name='version', desc='Shows the currently runnning version of the bot.')
+async def versionCommand(message, args):
+    await message.channel.send(f'Bot is running version {version}.')
 
 # Passive events (startup/etc.)
 
+@CommandHandler(name='test')
 async def testCommand(message, args):
-    print(args)
-commands.append(SimpleCommand(name='test', executor=testCommand))
+    print(commands[0].name + commands[0].desc)
+
+# commands.append(SimpleCommand(name='test', executor=testCommand))
 
 
 @client.event
@@ -142,26 +120,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     # If message is from a bot, ignore
     if message.author.bot:
         return
 
     # If message begins with command prefix, process like a command
     if message.content[0] == command_prefix:
-        # Process as command
+
         args = (message.content[1:]).split()
-        await commands[0].executor(message, args)
+
+        # Run any matching command executors to the arg0
+        for c in commands:
+            if c.name.lower() == args[0].lower():
+                await c.executor(message, args)
 
 
     # Check for any other message related events
 
-    # print("test")
-    # print(message.author.roles)
-
-    # for role in message.author.roles:
-    #     if role.name.lower() == "horse": # User has role
-    #         if random.randint(0, 20) == 0:
-    #             await message.reply(random.choice(["ok gabe", "heard", "over", "good comms", "ratio + homeless", "who asked tho"]))
+    for role in message.author.roles:
+        if role.name.lower() == "horse": # User has role
+            if random.randint(0, 20) == 0:
+                await message.reply(random.choice(["ok gabe", "heard", "over", "good comms", "ratio + homeless", "who asked tho"]))
 
     
 
